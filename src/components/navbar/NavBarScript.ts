4b1452a8496480d921ba4e9d530905ec828d4125
RAY <souradeep20roy01@gmail.com>
2023-12-04 02:17:54 +0530

    Updated darkMode toggle functionality

--- src/components/navbar/NavBarScript.ts
+++ src/components/navbar/NavBarScript.ts
@@ -1,18 +1,43 @@
+// Dark Mode Toggle
 const darkModeButton = document.querySelector(
   ".dark-mode-button"
 ) as HTMLButtonElement;
-const toggleSidebarButton = document.querySelector(
-  ".sidebar-toggle-button"
-) as HTMLButtonElement;
-const sideBar = document.getElementById("sidebar");
+type Themes = "light" | "dark";
 
+function saveTheme(theme: Themes) {
+  let date = new Date();
+  date.setTime(date.getTime() + 365 * 86400000);
+  document.cookie = `theme=${theme};expires=${date.toUTCString()}`;
+}
+
+const sideBar = document.getElementById("sidebar");
+const element = document.querySelector("html")?.classList;
+const theme = document.cookie.match("theme=([^;]+)");
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (theme && theme[1] === "dark") {
+    element?.add("dark");
+  }
+});
+
+darkModeButton.addEventListener("click", () => {
+  if (element?.contains("dark")) {
+    element.remove("dark");
+    saveTheme("light");
+  } else {
+    element?.add("dark");
+    saveTheme("dark");
+  }
+});
+
+// Sidebar Toggle
 function toggleClass(element: Element, className: string) {
   element.classList.toggle(className);
 }
 
-darkModeButton.addEventListener("click", () => {
-  toggleClass(document.documentElement, "dark");
-});
+const toggleSidebarButton = document.querySelector(
+  ".sidebar-toggle-button"
+) as HTMLButtonElement;
 
 toggleSidebarButton.addEventListener("click", () => {
   if (sideBar) {
