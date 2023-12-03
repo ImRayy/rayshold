4ea64ac6722a5cfc165ce6fb55d45e5faa6e2e15
RAY <souradeep20roy01@gmail.com>
2023-12-04 02:18:27 +0530

    Added @astrojs/vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,22 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   prefetch: {
-    defaultStrategy: "hover",
+    defaultStrategy: "hover"
   },
-  integrations: [
-    tailwind(),
-    mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: {
-        theme: "material-theme",
-      },
-    }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), mdx({
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "material-theme"
+    }
+  })],
+  output: "server",
+  adapter: vercel()
 });
