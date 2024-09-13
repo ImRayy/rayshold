3178f4180b33300bd88714ed35b175cb5cbb742e
ImRayy <souradeep_roy@pm.me>
2024-09-13 21:26:05 +0530

    perf: optimize image loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,32 +7,38 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    domains: ["ik.imagekit.io"],
+  },
   prefetch: true,
   prefetch: {
-    defaultStrategy: "hover"
+    defaultStrategy: "hover",
   },
   markdown: {
     shikiConfig: {
       theme: "css-variables",
-      langs: ["ts", "tsx", "go", "astro", "py", "lua"]
+      langs: ["ts", "tsx", "go", "astro", "py", "lua"],
     },
-    remarkPlugins: [remarkToc]
+    remarkPlugins: [remarkToc],
   },
   integrations: [
-  /**
-   * I'm currently using the manual method to initialize Swup to avoid the
-   * window.swup type error while globalInstance is set to true
-   * **/
-  // swup({
-  //   theme: false,
-  //   cache: true,
-  //   smoothScrolling: false,
-  //   accessibility: true,
-  //   globalInstance: true,
-  // }),
-  tailwind({
-    applyBaseStyles: false
-  }), mdx({
-    optimize: true
-  }), react()]
+    /**
+     * I'm currently using the manual method to initialize Swup to avoid the
+     * window.swup type error while globalInstance is set to true
+     * **/
+    // swup({
+    //   theme: false,
+    //   cache: true,
+    //   smoothScrolling: false,
+    //   accessibility: true,
+    //   globalInstance: true,
+    // }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    mdx({
+      optimize: true,
+    }),
+    react(),
+  ],
 });
