07c5caed33ea315ea6c394f0d6af66cfc090155b
ImRayy <souradeep_roy@pm.me>
2025-05-21 18:50:51 +0530

    chore: update to Astros new contentCollection API

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,32 +1,38 @@
+import { glob } from "astro/loaders";
 import { z, defineCollection } from "astro:content";
 
+const image = z.object({
+  src: z.string(),
+  alt: z.string(),
+  caption: z.string().optional(),
+});
+
+// -----------------------------------------
+// POST SCHEMA
+// -----------------------------------------
 const post = z.object({
   draft: z.boolean(),
   title: z.string(),
   description: z.string(),
   publishDate: z.date(),
   tags: z.array(z.string()),
-  image: z
-    .object({
-      src: z.string(),
-      alt: z.string(),
-      caption: z.string().optional(),
-    })
-    .optional(),
+  image: image.optional(),
 });
 
 const blogCollection = defineCollection({
-  type: "content",
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
   schema: post,
 });
 
 export type PostType = {
   id: string;
-  slug: string;
-  body: string;
+  body?: string;
   data: z.infer<typeof post>;
 };
 
+// -----------------------------------------
+// EXPORTS
+// -----------------------------------------
 export const collections = {
   blog: blogCollection,
 };
